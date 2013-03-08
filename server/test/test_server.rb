4da96da8c89463bda5d4b778e1fd3a113bff0a6b
Chris Schmich <schmch@gmail.com>
2013-03-08 15:08:10 -0800

    Adding simple test for /version.

--- server/test/test_server.rb
+++ server/test/test_server.rb
@@ -1,6 +1,7 @@
 require 'server'
 require 'test/unit'
 require 'rack/test'
+require 'json'
 
 set :environment, :test
 
@@ -20,5 +21,17 @@ class QcpServerTest < Test::Unit::TestCase
   def test_no_index
     get '/'
     assert_equal 404, last_response.status
+  end
+
+  def test_version
+    get '/version'
+    obj = assert_json_response
+    assert !obj['version'].nil?
+  end
+
+  def assert_json_response
+    assert last_response.ok?
+    assert last_response.content_type =~ /application\/json/
+    return JSON.parse(last_response.body)
   end
 end
